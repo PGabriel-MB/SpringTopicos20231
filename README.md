ba58f329a010b29f6eaa65ac4320ae717e9a8ab9
PGabriel-MB <pablogrbarbosa@gmail.com>
2023-02-14 10:03:38 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# SpringTopicos20231
---
+++ src/main/java/com/fatec/pablo/springtopicos20231/SpringTopicos20231Application.java
@@ -0,0 +1,13 @@
+package com.fatec.pablo.springtopicos20231;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTopicos20231Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTopicos20231Application.class, args);
+	}
+
+}
